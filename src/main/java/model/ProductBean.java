4ba9a1d4835e6ef54a336a5de609683fff938684
Lapushkin94 <62937994+Lapushkin94@users.noreply.github.com>
2020-10-24 01:50:07 +0300

    base functionality

--- src/main/java/model/ProductBean.java
+++ src/main/java/model/ProductBean.java
@@ -5,34 +5,29 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
 import javax.faces.bean.SessionScoped;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Data
-//@Named("productBean")
-//@SessionScoped
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Named("productBean")
+@SessionScoped
 public class ProductBean implements Serializable {
 
-    private List<ProductForStand> productList;
+    private List<ProductForStand> productList = new ArrayList<>();
 
     {
         ProductForStand productForStand = new ProductForStand();
-        productForStand.setName("TestName");
-        productForStand.setAuthor("TestAuthor");
-        productForStand.setPrice(99);
+        productForStand.setName("No data yet");
+        productForStand.setAuthor("No data yet");
+        productForStand.setPrice(0);
         productList.add(productForStand);
     }
 
-    public List<ProductForStand> getProductList() {
-        return productList;
-    }
-
-    public void setProductList(List<ProductForStand> productList) {
-        this.productList = productList;
-    }
 }
